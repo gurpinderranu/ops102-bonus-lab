3fe0e0191a59e1b763624a123f7cbbdfb008f5db
Gurpinder Singh Ranu <gsranu2@mtrx-node03pd.dcm.senecacollege.ca>
2026-04-15 13:24:30 -0400

    modified the script (testlab.sh)

--- testlab.sh
+++ testlab.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# OPS102 Bonus Lab - Software Testing Script
-# - IPC144 Lab 6 source files used for testing
-# - GenAI used: ChatGPT
-# - Prompt used:
-#   Help me with guidance for building and understanding how to create a Bash script to test my C program using input and expected output files.
-
 PROGRAM="program"
 PASS_COUNT=0
 TOTAL_TESTS=4
